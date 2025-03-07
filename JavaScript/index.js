0672653e78f331a1c0e6a3a54c15480c2fecdd91
Chetan-Troopy <chetan.sharma@troopystack.com>
2025-03-07 12:26:49 +0530

    Lecture Data

---
+++ JavaScript/index.js
@@ -0,0 +1,53 @@
+// Variables In JS 
+
+// var   => It was used before ES6
+// let   => Introduce In ES6
+// const => constant 
+
+// 1. Var 
+
+
+// keyword Name = Value Assign (Data Type)
+
+
+// var a;
+// a= 10;
+
+// var a = 10;  // Initialization and Declaration 
+// var a = 20;   // Re-Declaration is Possible 
+// a = 30;       // Re-Assignment is Possible
+// console.log(a);
+
+
+// 2. let
+// let name = "Manav";  // Initialization and Declaration 
+// // let name = "Manav Patel";   // Re-Declaration is Impossible 
+// name = "Manav Parikh";       // Re-Assignment is Possible
+// console.log(name);
+
+
+// 3. const 
+
+// const fav_Cricketer = "Rohit Sharma";
+// // const fav_Cricketer = "Ishan Kishan";    // Re-Declaration is Impossible 
+// // fav_Cricketer = "Virat Kohli";            // Re-Assignment is Impossible
+// console.log(fav_Cricketer);
+
+
+
+// Data Types 
+
+// 1. Premitive Data Type
+
+// Number
+// null
+// String
+// Symbol
+// BigInt
+// Boolean
+// undefined
+
+// 2. Non-Premitive Data Type
+
+// Array
+// Object 
