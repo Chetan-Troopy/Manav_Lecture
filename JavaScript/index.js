1fe47dd9befcd8c9ce38145f47fd03bf324b6c98
Chetan-Troopy <chetan.sharma@troopystack.com>
2025-03-28 12:03:29 +0530

    Lecture Data

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,41 +1,95 @@
-// switch Case
+// Higher Order Function
 
-// Syntax 
+// -> Higher-order functions are functions that either take one 
+// or more functions as arguments, or return a function as their
+//  result, enabling powerful abstractions and code reuse. 
 
-// switch (key) {
-//     case value:
-//         // code to be executed 
-//         break;
-
-//     default:
-//         // code to be executed 
-//         break;
-// }
+// -> A function takes another function as an Arguments
+    // is a Higher Order Function 
 
 
-let a = 4;
+    // Map 
+
+    // The map() method of Array instances creates a new array 
+    // populated with the results of calling a provided function 
+    // on every element in the calling array.
 
 
-switch (a) {
-    case 1:
-        console.log("one");
+
+    const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    // num.map((e)=>{
+    //     console.log(e);
         
-        break;
-    case 2:
-        console.log("two");
-        
-        break;
-    case 3:
-        console.log("three");
-        
-        break;
-    case 4:
-        console.log("four");
-        
-        break;
+    // })
 
-    default:
-        console.log("enter valid number");
+    // const doubled = num.map(num => num * 2);
+    // const doubled = num.map((e) =>{
+    //     e = e*2
+    //     // console.log(e);
         
-        break;
-}
+    // });
+
+    // console.log(doubled);
+    
+    
+    // Filter 
+
+
+    // const even = num.filter(num => num % 2 === 0);
+
+    // console.log(even);
+
+    // Reduce 
+
+    console.log(num);
+    
+
+    const sum = num.reduce((acc, num) => acc + num, 0)
+
+    console.log(sum);
+    
+
+
+
+
+// Callback Function 
+
+// -> A callback function is a function passed into another function 
+// as an argument, which is then invoked inside the outer function to 
+// complete some kind of routine or action
+
+// -> A function passed into another function as an Arguments
+    // is a Callback Function 
+
+//    function a(b) {                 // HOF
+//         b();
+//         console.log("a");
+//       }
+
+//       a(function b() {              // Callback
+//         console.log("b");
+//       });
+
+
+    // console.log("Good Morning");
+    // setTimeout(() => {
+    //   console.log("Hello");
+      
+    // }, 5000);
+    // console.log("Good Afternoon");
+    // console.log("Good Night");
+    
+
+
+    // ->   Time and Tide and JavaScript Wait for none
+
+
+
+
+    // setInterval(() => {
+    //     console.log("Namaste");
+        
+    // }, 2000);
+
+
